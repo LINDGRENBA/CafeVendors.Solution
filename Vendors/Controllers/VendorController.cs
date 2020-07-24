a94f76184df157af3a7fd4953829143ad3662f51
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-07-24 15:39:39 -0700

    Add form to create new Vendor object, update VendorController with route to post form.

--- Vendors/Controllers/VendorController.cs
+++ Vendors/Controllers/VendorController.cs
@@ -20,6 +20,13 @@ namespace CafeVendors.Controllers
     {
       return View();
     }
+
+    [HttpPost("/vendors")]
+    public ActionResult Create(string vendorName, string vendorDescription)
+    {
+      Vendor newVendor = new Vendor(vendorName, vendorDescription);
+      return RedirectToAction("Index"); //need to get this to route back to home page
+    }
   }
   
 }
